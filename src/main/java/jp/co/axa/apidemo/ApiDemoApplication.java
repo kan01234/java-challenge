ecfdd8e002e6e2d385a0e72be5f0c9e9c9f3ad4b
kan01234 <safghjkl@gmail.com>
2023-03-14 16:36:21 +0900

    manage by schema.sql

--- src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
+++ src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
@@ -1,15 +1,63 @@
 package jp.co.axa.apidemo;
 
+import io.r2dbc.spi.ConnectionFactory;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
-@EnableSwagger2
-@SpringBootApplication
+/** Api Demo Application */
+@SpringBootApplication(exclude = {ReactiveUserDetailsServiceAutoConfiguration.class})
+@EnableR2dbcRepositories
+@EnableWebFlux
+@EnableCaching
+@ImportAutoConfiguration(classes = {CacheAutoConfiguration.class, RedisAutoConfiguration.class})
+@OpenAPIDefinition(
+    info =
+        @Info(
+            title = "API Demo application",
+            version = "0.0",
+            description = "API Demo application",
+            contact =
+                @Contact(
+                    url = "https://github.com/kan01234",
+                    name = "Kan",
+                    email = "safghjkl@gmail.com")))
 public class ApiDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiDemoApplication.class, args);
-	}
+  /**
+   * initialize DB schema
+   *
+   * @param connectionFactory the connection factory
+   * @return the connection factory initializer
+   */
+  @Bean
+  public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+    ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
+    initializer.setConnectionFactory(connectionFactory);
+    initializer.setDatabasePopulator(
+        new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+    return initializer;
+  }
 
+  /**
+   * Main
+   *
+   * @param args the args
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ApiDemoApplication.class, args);
+  }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    salary INT,
+    department VARCHAR(255)
+);
